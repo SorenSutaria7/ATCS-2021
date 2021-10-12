fb0dc5be03010d4a9cd3cfa295f0944b16dd000f
Soren Sutaria <sorensutaria@gmail.com>
2021-10-12 11:15:34 -0700

    Functions Exercises

---
+++ project0/addCalc.py
@@ -0,0 +1,7 @@
+def addition_calculator(num1, num2):
+    print(str(num1) + "+" + str(num2) + "=" + str(num1+num2))
+
+addition_calculator(5,7)
+addition_calculator(10,10)
+addition_calculator(1,100)
+
