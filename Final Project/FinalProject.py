5700ba1f8cdf7fcc5774cd23c06e79cf03e80646
Soren Sutaria <sorensutaria@gmail.com>
2022-05-03 14:58:29 -0700

    Accepts in the genre files containing audio files and converts them to .wav files

--- Final Project/FinalProject.py
+++ Final Project/FinalProject.py
@@ -1,5 +1,29 @@
-import numpy as np
-import pandas as pd
-import scipy.io.wavfile as wav
-from python_speech_features import mfcc
+# import numpy as np
+# import pandas as pd
+# from python_speech_features import mfcc
+# import scipy.io.wavfile as wav
+# from python_speech_features import logfbank
+import sox
+
+import os
+
+genres = "/test_genre"
+
+for current_genre in os.walk(genres):
+    os.chdir(current_genre)
+    for audio_file in os.listdir(current_genre):
+        os.system("sox " + str(audio_file) + " " + str(audio_file[:-3]) + ".wav")
+    os.system("rm *.au")
+
+
+
+
+
+
+
+
+
+
+
+
 
